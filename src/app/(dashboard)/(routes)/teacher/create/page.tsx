d2a0650ad4472d26ae4d3848bee689273fe8377d
Ilya <80617082+imatyushov@users.noreply.github.com>
2024-01-31 11:59:14 +0300

    Страница создания курса

--- src/app/(dashboard)/(routes)/teacher/create/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -1,9 +1,10 @@
-'use client';
+"use client";
 
-import * as z from 'zod';
+import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 import {
     Form,
@@ -12,56 +13,86 @@ import {
     FormField,
     FormLabel,
     FormMessage,
-    FormItem
-} from '@/components/ui/form';
+    FormItem,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 const formSchema = z.object({
     title: z.string().min(1, {
-        message: 'Название необходимо'
-    })
+        message: "Title is required",
+    }),
 });
 
-export default function Page() {
+export default function CreatePage()  {
+    const router = useRouter();
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
-            title: ''
-        }
+            title: ""
+        },
     });
 
     const { isSubmitting, isValid } = form.formState;
 
-    const onSubmit = (values: z.infer<typeof formSchema>) => {
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
         console.log(values);
     }
+
     return (
-        <div className='max-w-5xl mx-auto flex md:items-center md:justify-center h-full m-3'>
-           <div>
-               <h1 className='text-2xl'>
-                   Введите название курса
-               </h1>
-               <p className='text-sm text-slate-600'>Как вы хотите назвать ваш курс?</p>
-               <Form {...form}>
-                   <form
-                       className='space-y-8 mt-8'
-                       onSubmit={form.handleSubmit(onSubmit)}>
-                       {/*<FormField*/}
-                       {/*    name='title'*/}
-                       {/*    render={({ field }) => {*/}
-                       {/*        <FormItem>*/}
-                       {/*            <FormLabel>*/}
-                       {/*                Название курса*/}
-                       {/*            </FormLabel>*/}
-                       {/*            <FormControl>*/}
-                       {/*                <Input></Input>*/}
-                       {/*            </FormControl>*/}
-                       {/*        </FormItem>*/}
-                       {/*    }}*/}
-                       {/*>*/}
-                       {/*</FormField>*/}
-                   </form>
-               </Form>
-           </div>
+        <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+            <div>
+                <h1 className="text-2xl">
+                    Название курса
+                </h1>
+                <p className="text-sm text-slate-600">
+                    Назовите ваш курс, далее вы сможете изменить название
+                </p>
+                <Form {...form}>
+                    <form
+                        onSubmit={form.handleSubmit(onSubmit)}
+                        className="space-y-8 mt-8"
+                    >
+                        <FormField
+                            name="название"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Course title
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input
+                                            disabled={isSubmitting}
+                                            placeholder="Фронтенд курс"
+                                            {...field}
+                                        />
+                                    </FormControl>
+                                    <FormDescription>
+                                        Что входит в программу обучения?
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <div className="flex items-center gap-x-2">
+                            <Link href="/">
+                                <Button
+                                    type="button"
+                                    variant="ghost"
+                                >
+                                    Отменить
+                                </Button>
+                            </Link>
+                            <Button
+                                type="submit"
+                                disabled={!isValid || isSubmitting}
+                            >
+                                Продолжить
+                            </Button>
+                        </div>
+                    </form>
+                </Form>
+            </div>
         </div>
-    )
+    );
 }
